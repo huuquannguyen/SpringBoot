64f63f760e51acec5fec7aba0b7dc80e2d046590
Nguyen Huu Quan <huuquan.nguyen.qt@gmail.com>
2021-06-20 19:01:12 +0700

    bai tap spring boot 1

---
+++ films/src/main/java/vn/techmaster/films/controller/HomeController.java
@@ -0,0 +1,39 @@
+package vn.techmaster.films.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import vn.techmaster.films.model.Film;
+
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping()
+    public String showHomePage(Model model){
+        model.addAttribute("name", "Quan");
+        return "index";
+    }
+
+    @GetMapping("about")
+    public String showAboutPage(Model model){
+        model.addAttribute("name", "Quan Huu");
+        return "about";
+    }
+
+    @GetMapping("films")
+    public String listFilm (Model model){
+        List<Film> films = List.of(
+            new Film("BÀN TAY DIỆT QUỶ", "Hành Động", " Park Seo Joon", 2021),
+            new Film("GODZILLA VS KONG", "Hành Động", "Millie Bobby Brown", 2021),
+            new Film("MORTAL KOMBAT: CUỘC CHIẾN SINH TỬ", "Hành Động", "Lewis Tan", 2021)
+        );
+        model.addAttribute("films", films);
+        return "film";
+    }
+}
